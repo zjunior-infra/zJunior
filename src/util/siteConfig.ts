4155944301124346d2b498cb24e11f6f4bf9af44
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-15 22:09:06 +0200

    prepering the layouts

---
+++ src/util/siteConfig.ts
@@ -0,0 +1,9 @@
+export const siteConfig={
+    title: 'Jobs for Entryleve and students',
+    description: 'opprtunity for students and fresh-graduates to find jobs in MEAN and Egypt',
+    keywords:[
+        'jobs',
+        'internships',
+        'intern',
+    ],
+}
