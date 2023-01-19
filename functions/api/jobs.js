ff0d632f622ca9c5c445eb3150afaef3de59a137
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-19 20:07:52 +0200

    trying the cloudflare functions

--- functions/api/jobs.js
+++ functions/api/jobs.js
@@ -1,6 +1,23 @@
 import {PrismaClient} from '@prisma/client/edge'
+import {Router, listen} from 'worktop'
+
 const prisma= new PrismaClient()
-export async function onRequestGet(context){
-    const jobs= await prisma.job.findMany({})
-    return new Response(JSON.stringify(jobs))
+
+const API=new Router();
+
+API.add('GET','/api/hello',async (req,ctx)=>{
+    new Response("HIIIII bitch")
+})
+
+export default{
+    async fetch(req,env,ctx){
+        const url =new URL(req.url)
+        if(url.pathname.startsWith('/api/')){
+            ctx.bindings=env;
+            // @ts-ignore
+            return API.run(req,ctx)
+        }
+
+        return env.ASSETS.fetch(req);
+    }
 }
