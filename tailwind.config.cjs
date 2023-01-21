ad791a461d94366ca715d1f34b9e1507c6a679f3
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-21 19:26:40 +0200

    completing the from and ready for POST

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	],
 	theme: {
 		extend: {},
 		container:{
 			center:true
 		}
 	},
-	plugins: [],
+	plugins: [
+	],
 }
