e1e37d87676e8f220ff8c9f45b193d75d1baa55f
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-23 18:21:02 +0200

    pages functions

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,7 @@
-import {PrismaClient} from '@prisma/client/edge.js'
+import {PrismaClient} from '@prisma/client/edge'
 import {Router, listen} from 'worktop'
 
-
+const prisma = new PrismaClient()
 const API=new Router();
 
 API.add('GET','/',async (req,ctx)=>{
@@ -12,7 +12,6 @@ export default{
     async fetch(req,env,ctx){
         const url =new URL(req.url)
             ctx.bindings=env;
-            console.log("1")
             // @ts-ignore
             return API.run(req,ctx)
 
