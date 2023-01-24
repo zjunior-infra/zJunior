ae58b82d1ba69e1154dfc27e4a98d3bd53e98f61
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-24 02:01:17 +0200

    config env to cf

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,12 @@ import react from "@astrojs/react";
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+		define: {
+			"process.env.DATABASE_URL": JSON.stringify(
+				process.env.DATABASE_URL,
+			)
+		},
+	},
 });
