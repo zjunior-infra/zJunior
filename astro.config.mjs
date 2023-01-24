89c869eb94a7c67a88325f6adad24dfbb1e60552
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-24 02:21:54 +0200

    change the mode of cf adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({mode:"directory"}),
+  adapter: cloudflare({mode:'directory'}),
   integrations: [tailwind(), react()],
   vite: {
 		define: {
