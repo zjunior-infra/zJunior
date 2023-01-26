67a6d2e27ec002762dc078e4b24987c00e731a1e
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-26 23:51:54 +0200

    Fixing env to be exposed on CF pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,4 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare({mode:'directory'}),
   integrations: [tailwind(), react()],
-  vite: {
-		define: {
-			"process.env.DATABASE_URL": JSON.stringify(
-				process.env.DATABASE_URL,
-			)
-		},
-	},
 });
