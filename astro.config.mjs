ebdc4d94adce59ae8432075ed9bce85e731cf3b2
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-27 02:06:56 +0200

    changing env

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare({mode:'directory'}),
   integrations: [tailwind(), react()],
+  vite: {
+    define: {
+      'process.env.DATABASE_URL': process.env.DATABASE_URL,
+    },
+  },
 });
