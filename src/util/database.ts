16cee64fee350d693a6e5844f908cee3c10f3664
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-27 02:32:46 +0200

    change the env approach for CF

--- src/util/database.ts
+++ src/util/database.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client/edge"
 export const prisma=new PrismaClient({
     datasources:{
       db:{
-        url: process.env.DATABASE_URL
+        url: "process.env.DATABASE_URL"
       }
     }
 })
