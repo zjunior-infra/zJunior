ef7d85c860c7221c1cf45bae79924b1b5e6913b6
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-28 01:29:16 +0200

    adding partytown with googleTags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({mode:'directory'}),
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), partytown({
+    config: { debug: false },
+    // @ts-ignore
+    forward: ["dataLayer.push"],
+  })],
   vite: {
     define: {
       "process.env.DATABASE_URL": process.env.DATABASE_URL,
--- src/util/database.ts
+++ src/util/database.ts
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client/edge"
-import type { IJob } from "./job.interface";
 
 export const prisma=new PrismaClient({
     datasources:{
       db:{
-        url: "prisma://aws-eu-central-1.prisma-data.com/?api_key=bvx0KupXi-9xOR1olnBofFh9c1Qm__NV5lnfre9Fzp7R8d_s62O_fLGOsYGGETXn"
+        url: "process.env.DATABASE_URL"
       }
     }
 })
