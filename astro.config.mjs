4328fcdb93e06a6d3d0be1bd619bd9b537c67508
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-01-28 02:53:09 +0200

    Removing partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import partytown from '@astrojs/partytown';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({mode:'directory'}),
-  integrations: [tailwind(), react(), partytown({
-    config: { debug: false },
-    //@ts-ignore
-    forward: ["dataLayer.push"],
-  })],
+  integrations: [tailwind(), react()],
   vite: {
     define: {
       "process.env.DATABASE_URL": process.env.DATABASE_URL,
