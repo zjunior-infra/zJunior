e2230cd436955a72d6fe0fbd0783e6b656786d7b
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-02-03 03:28:15 +0200

    Compressing the build size, for more faster performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import compress from "astro-compress";
 
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({mode:'directory'}),
-  integrations: [tailwind(), react()],
+  adapter: cloudflare({
+    mode: 'directory'
+  }),
+  integrations: [tailwind(), react(), compress()],
   vite: {
     define: {
       "process.env.DATABASE_URL": process.env.DATABASE_URL,
-	    "process.env.DEV_DATABASE_URL": process.env.DEV_DATABASE_URL,
-    },
-  },
+      "process.env.DEV_DATABASE_URL": process.env.DEV_DATABASE_URL
+    }
+  }
 });
