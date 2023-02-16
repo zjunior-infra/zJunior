ff829b8fc9526c1961c8b3fc2d7d2595a78c198f
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-02-16 03:47:49 +0200

    change the type of vite

--- src/pages/api/cleanJobs.ts
+++ src/pages/api/cleanJobs.ts
@@ -2,11 +2,11 @@ import type { APIRoute } from "astro";
 import { cleaningJobs } from "@util/database"
 
 export const post: APIRoute = async ({ request }) => {
-        const reqAuth = request.headers.get('auth');
+        const reqAuth:string = request.headers.get('auth');
         try{
             if (!reqAuth)
             return new Response("Not Authorized", {status:401})
-            if(reqAuth==="process.env.DEL_TOKEN")
+            if(reqAuth==("process.env.DEL_TOKEN"))
             {
                 const result = await cleaningJobs();
                 return new Response(result,{status:200});
@@ -19,10 +19,4 @@ export const post: APIRoute = async ({ request }) => {
             return new Response(err.message,{status:500})
         }
         
-}
-export const get: APIRoute = async ({ request }) => {
-    const reqAuth = request.headers.get('auth');
-    
-    return new Response(reqAuth,{status:200})
-    
 }
