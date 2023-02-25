78fd2c829e8a8ae8210205243bfd28b6504e3200
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-02-25 06:06:59 +0200

    Remove unused code for querying jobs

--- src/pages/api/jobs.json.ts
+++ src/pages/api/jobs.json.ts
@@ -1,122 +1,7 @@
 import { prisma,formatJobs } from "@util/database";
 import type { APIContext, APIRoute } from "astro";
-function customizeQuery(query){
-    const split=query.split(' ')
-    const first=split[0];
-    const second=split[1];
-    return `+${first} +${second}`
-}
 
-async function getQueredData(query='',jobtype='',tags=''){
-    if(!query && !jobtype && !tags){
-        try{
-            const result= await prisma.job.findMany()
-            return formatJobs(result);
-        }
-        catch(err){
-            return err.message;
-        }
-    }
-    if(!jobtype && !tags && query){
-        try{
-            const result = await prisma.job.findMany({
-            where:{
-                title:{
-                    search:customizeQuery(query)
-                    }
-                }
-            });
-            return formatJobs(result)
-        }
-        catch(err){
-            return err.message
-        }
-    }
-    if(jobtype && !tags && !query){
-        try{
-            const result = await prisma.job.findMany({
-            where:{
-                type:{
-                    equals:jobtype
-                    }
-                }
-            });
-            return formatJobs(result)
-        }
-        catch(err){
-            return err.message
-        }
-    }
-    if(!jobtype && tags && !query){
-        try{
-            const result = await prisma.job.findMany({
-            where:{
-                skills:{
-                    search:tags
-                    }
-                }
-            });
-            return formatJobs(result)
-        }
-        catch(err){
-            return err.message
-        }
-    }
-    if(jobtype && !tags && query){
-        try{
-            const result = await prisma.job.findMany({
-            where:{
-                title:{
-                    search:customizeQuery(query)
-                    },
-                type:{
-                    equals:jobtype
-                },
-                }
-            });
-            return formatJobs(result)
-        }
-        catch(err){
-            return err.message
-        }
-    }
-    if(!jobtype && tags && query){
-        try{
-            const result = await prisma.job.findMany({
-            where:{
-                title:{
-                    search:customizeQuery(query)
-                    },
-                skills:{
-                    search:tags
-                },
-                }
-            });
-            return formatJobs(result)
-        }
-        catch(err){
-            return err.message
-        }
-    }
-    if(jobtype && tags && !query){
-        try{
-            const result = await prisma.job.findMany({
-            where:{
-                type:{
-                    equals:jobtype
-                    },
-                skills:{
-                    search:tags
-                },
-                }
-            });
-            return formatJobs(result)
-        }
-        catch(err){
-            return err.message
-        }
-    }
-}
+
 
 export const get: APIRoute = async (context: APIContext) => {
     const crosHeaders={
@@ -127,18 +12,10 @@ export const get: APIRoute = async (context: APIContext) => {
     }
     
     try {
-            const jobtype = context.url.searchParams.get('jobtype') ? context.url.searchParams.get('jobtype'): ''
-            const tags = context.url.searchParams.get('tags') ? context.url.searchParams.get('tags'): ''
-            const query = context.url.searchParams.has('query') ? context.url.searchParams.get('query'): ''
-            
-            const data=await getQueredData(query,jobtype,tags)
-            return new Response(JSON.stringify(data),{status:200, headers:crosHeaders})
-            
-        // }
-        // else{
-            // return new Response('Sorry You Cannot Get The Data',{status:401 , headers:crosHeaders})
-        // }
-    } catch (e: any) {
+        const result= await prisma.job.findMany()
+        return new Response(JSON.stringify(formatJobs(result)),{status:200, headers:crosHeaders})
+    }
+    catch (e: any) {
         return new Response(e.message, { status: 500 });
     }
 }
