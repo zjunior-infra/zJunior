76dc457a98c5e47803338f133637e1d2696a481c
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-08-11 05:14:41 +0300

    revampe the navigation

--- src/components/UI/3DButton.tsx
+++ src/components/UI/3DButton.tsx
@@ -3,7 +3,7 @@ import {cn} from '@util/utils'
 import React from 'react'
 
 const buttonVariants = cva(
-    "relative inline-block justify-center z-[10] items-center rounded-md text-sm font-medium active:scale-95 transition-all duration-300 disabled:pointer-events-none disabled:opacity-50 bg-white after:absolute after:w-[100%]  after:flex after:justify-center after:items-center after:h-[100%] after:z-[0] after:bg-white after:inset-0 after:bg-green-600 after:border-2  after:rounded-md after:-translate-y-[0.2rem] after:-translate-x-[0.2rem] after:duration-300 after:hover:translate-y-0 after:hover:translate-x-0",
+    "relative inline-block justify-center items-center text-accent",
     {
         variants: {
           size: {
