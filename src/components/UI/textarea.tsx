0f0e1f3771ca4f1af47b78f76612fa5be5b919a7
Hamed Osama <hamedosama19@gmail.com>
2023-08-16 01:49:12 +0300

    add error styling

--- src/components/UI/textarea.tsx
+++ src/components/UI/textarea.tsx
@@ -3,14 +3,17 @@ import * as React from "react"
  
  
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+    error?: boolean
+  }
  
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, error, ...props }, ref) => {
     return (
       <textarea
         className={cn(
           "flex min-h-[80px] w-full text-foreground rounded-md z-[400] bg-muted px-3 py-2 text-sm ring-offset-background placeholder:text-foreground/30 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+          error && "ring-2 ring-red-600 focus-visible:ring-red-600",
           className
         )}
         ref={ref}
